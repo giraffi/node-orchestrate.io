04c76478d00c035239f0ce7be6bf3414766c11b1
azukiwasher <azukiwasher@yahoo.co.jp>
2013-12-26 19:19:23 +0900

    Add tests for search, getEvent and putEvent functions

--- test/orchestrate.io/client-test.js
+++ test/orchestrate.io/client-test.js
@@ -27,14 +27,14 @@ describe('orchestrate.io/client', function () {
         server = helper.fakeIo.keyValue({
           method: 'GET',
           collection: 'films',
-          key: 'rashomon'
+          key: 'kurosawa'
         });
       }
 
-      client = helper.io.createClient({apikey: 'abc'});
+      client = helper.io.createClient({ apikey: 'abc' });
       client.getKeyValue({
         collection: 'films',
-        key: 'rashomon'
+        key: 'kurosawa'
       }, function (err, data) {
         expect(err).to.be.null;
         expect(data.Title).to.equal('Rashomon');
@@ -51,15 +51,15 @@ describe('orchestrate.io/client', function () {
         server = helper.fakeIo.keyValue({
           method: 'PUT',
           collection: 'films',
-          key: 'rashomon',
+          key: 'kurosawa',
           data: data
         });
       }
 
-      client = helper.io.createClient({apikey: 'abc'});
+      client = helper.io.createClient({ apikey: 'abc' });
       client.putKeyValue({
         collection: 'films',
-        key: 'rashomon',
+        key: 'kurosawa',
         data: data
       }, function (err, data) {
         expect(err).to.be.null;
@@ -69,4 +69,81 @@ describe('orchestrate.io/client', function () {
     });
   });
 
+  describe('.search', function () {
+    it('performs GET Search successfully', function () {
+      var queryString = 'Genre:crime';
+
+      if (mock) {
+        server = helper.fakeIo.search({
+          collection: 'films',
+          query: queryString
+        })
+      }
+
+      client = helper.io.createClient({ apikey: 'abc' });
+      client.search({
+        collection: 'films',
+        query: 'Genre:crime'
+      }, function (err, data) {
+        expect(err).to.be.null;
+        expect(data.results[0].value.Genre).to.equal('Crime, Drama');
+        server && server.done();
+      });
+    });
+  });
+
+  describe('.getEvent', function () {
+    it('performs GET Events successfully', function () {
+      if (mock) {
+        server = helper.fakeIo.event({
+          method: 'GET',
+          collection: 'films',
+          key: 'kurosawa',
+          type: 'comments',
+          start: 1388036932,
+          end: 1388040903
+        });
+      }
+
+      client = helper.io.createClient({ apikey: 'abc' });
+      client.getEvent({
+        collection: 'films',
+        key: 'kurosawa',
+        type: 'comments',
+        start: 1388036932,
+        end: 1388040903
+      }, function (err, data) {
+        expect(err).to.be.null;
+        expect(data.count).to.equal(1);
+        server && server.done();
+      });
+    })
+  });
+
+  describe('.putEvent', function () {
+    it('performs PUT Events successfully', function () {
+      var data = JSON.stringify({ note: "magnifique!" });
+      if (mock) {
+        server = helper.fakeIo.event({
+          method: 'PUT',
+          collection: 'films',
+          key: 'kurosawa',
+          type: 'comments',
+          timestamp: 1388036932,
+        });
+      }
+
+      client = helper.io.createClient({apikey: 'abc'});
+      client.putEvent({
+        collection: 'films',
+        key: 'kurosawa',
+        type: 'comments',
+        timestamp: 1388036932
+      }, function (err, data) {
+        expect(err).to.be.null;
+        expect(data).to.be.empty;
+        server && server.done();
+      });
+    });
+  });
 });
